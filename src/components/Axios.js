dfde526867ec4c05844b9656ee93e03f1fa1ab5f
Shan Kuriakose <112441403+shankuriakose@users.noreply.github.com>
2023-11-30 11:53:43 +0530

    Update Axios.js

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = ""https://shankuriakose.pythonanywhere.com/";
+const baseUrl = "https://shankuriakose.pythonanywhere.com/";
 const AxiosInstance = axios.create({
   baseURL: baseUrl,
   timeout: 5000,
